7825cc4ca653166b577b6bb3b3d202231471e0ef
Dan Ventura <danwventura@icloud.com>
2016-03-26 13:43:27 -0500

    Added XHR and pushing objects to private array Used foreach to push each object into my private array

--- carlot.js
+++ carlot.js
@@ -1,1 +1,45 @@
 "use strict"
+
+
+
+
+//*******Original iife********//
+var CarLot = (function(){
+  // Array to push JSON object into//
+  let carArray = [];
+
+return{
+  loadInventory: function (){
+
+//*******Create an XHR object*******//  
+    let carRequest = new XMLHttpRequest();
+
+//*******XHR objects emit events when operation is complete*******//  
+    carRequest.addEventListener("load", function(){
+
+//*******Parsing JSON and looping looping through JS object to push each object into private array*******//  
+      let newCarObj = (JSON.parse(this.responseText).cars);
+        newCarObj.forEach(function(oneXHR){
+        carArray.push(oneXHR);
+        console.log("array", carArray);
+
+    })
+  })
+//*******Telling XHR *******//  
+    carRequest.open("GET", "inventory.json");
+    carRequest.send();
+}
+
+
+
+
+
+}
+
+
+
+
+
+
+})(CarLot);
+CarLot.loadInventory();
