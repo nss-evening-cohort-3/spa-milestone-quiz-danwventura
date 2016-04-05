e6e029eb511da6ba03998b7e9ddcbe8d4b3e69ae
Dan Ventura <danwventura@icloud.com>
2016-04-05 00:09:15 -0500

    outputting all car info to DOM successfully

--- eventhandlers.js
+++ eventhandlers.js
@@ -2,10 +2,25 @@
 var CarLot = (function(originalCarLot){
   
   originalCarLot.populatePage = function(carArray) {
-  
+    let carContainer = document.getElementById("container")
 
   // Loop over the inventory and populate the page
-console.log("array", carArray);
+    let outputString = "";
+    for (let i = 0; i < carArray.length; i++) {
+      let currentCar = carArray[i]
+      // console.log("curr", currentCar );
+      outputString += `<div id="eachCar${i}" class="col-md-4">${currentCar.make}`
+      outputString += `${currentCar.model}`
+      outputString += `${currentCar.year}`
+      outputString += `${currentCar.price}`
+      outputString += `${currentCar.color}`
+      outputString += `${currentCar.purchase}`
+      outputString += `${currentCar.description}`
+      outputString += `</div>`
+      // console.log("output", outputString);
+      carContainer.innerHTML = outputString;
+  }
+// console.log("array", carArray);
   // Now that the DOM is loaded, establish all the event listeners needed
 //   CarLot.activateEvents();
 }
