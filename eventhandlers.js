a40c22e75e4c6c16ef26bddaadf7f8f6a831041e
Dan Ventura <danwventura@icloud.com>
2016-04-11 22:24:22 -0500

    added comments and built DOM string

--- eventhandlers.js
+++ eventhandlers.js
@@ -1,31 +1,29 @@
 "use strict"
+//*******Establishing Augmenter*******//  
 var CarLot = (function(originalCarLot){
+//*******populatePage function builds DOM Strings*******//  
   
   originalCarLot.populatePage = function(carArray) {
-    let carContainer = document.getElementById("container")
+    let carContainer = document.getElementById("container");
 
-  // Loop over the inventory and populate the page
+  //*******Loops over inventory and populates DOM*******//  
     let outputString = "";
     for (let i = 0; i < carArray.length; i++) {
       let currentCar = carArray[i]
-      // console.log("curr", currentCar );
-      outputString += `<div id="eachCar${i}" class="col-md-4">${currentCar.make}`
-      outputString += `${currentCar.model}`
-      outputString += `${currentCar.year}`
-      outputString += `${currentCar.price}`
-      outputString += `${currentCar.color}`
-      outputString += `${currentCar.purchase}`
-      outputString += `${currentCar.description}`
+      
+  // *************Building DOM String*********** //
+      outputString += `<div id="eachCar${i}" class="col-md-4" style="border: solid 4px ${currentCar.color}">`
+      outputString += `<h2>${currentCar.year} ${currentCar.make} ${currentCar.model}</h2>`
+      outputString += `<h6> Price: ${currentCar.price} </h6>`
+      outputString += `<h5 id="${currentCar.color}"> Color: ${currentCar.color}</h5>`
+      outputString += `<h6> Availability: ${currentCar.purchased}</h6>`
+      outputString += `<h6> Desc: ${currentCar.description}</h6>`
       outputString += `</div>`
-      // console.log("output", outputString);
       carContainer.innerHTML = outputString;
   }
-// console.log("array", carArray);
-  // Now that the DOM is loaded, establish all the event listeners needed
-//   CarLot.activateEvents();
+  
+// Calling activateEvents which builds columns/rows
+  CarLot.activateEvents();
 }
 return originalCarLot
 })(CarLot || {});
-
-// Load the inventory and send a callback function to be
-// invoked after the process is complete
