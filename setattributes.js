744e11f1106b011223330dcc41fb2361c62b884e
Dan Ventura <danwventura@icloud.com>
2016-04-17 21:15:50 -0500

    removed console logs and added function to clear input/mirror input values to car description

--- setattributes.js
+++ setattributes.js
@@ -2,7 +2,7 @@
 
 //*******Border/Color Augmenter Iife********//
 var CarLot = (function(originalCarLot){
-  
+  let inputField = document.getElementById("inputField")
 
 
   originalCarLot.activateEvents = function(){
@@ -14,25 +14,53 @@ var CarLot = (function(originalCarLot){
     //*******Adding event listener to apply/remove styling to targeted Div********//
       theDiv.addEventListener("click", function(){
       let carColor = theDiv.querySelector("h5").id;
-      // console.log("div", carColor);
-      //*******Calling Reset function to reset all borders********//
-      originalCarLot.resetAtt(theDiv, carColor);
+
+    //*******Calling Reset function to reset all borders********//
+      originalCarLot.resetAtt(theDiv, carColor, allDivs);
+// define allDIvs above and pass to resetAtt and loop through to remove class from all divs
 
       })
     }
 },
 
   //*******Function to reset all borders/backgrounds********//
-  originalCarLot.resetAtt = function(theDiv, carColor){
-// carColor.classList.add
+  originalCarLot.resetAtt = function(theDiv, carColor, allDivs){
+    for (let i = 0; i < allDivs.length; i++) {
+      let defaultDivs = allDivs[i]
+      defaultDivs.classList.remove("selected")
+    }
 
+
+      originalCarLot.setAtt(theDiv, carColor);
   },
   
 
 //*******Function to change targeted Div's border/background********//
-  function setAtt (){
+  originalCarLot.setAtt = function(theDiv, carColor){
     
+    theDiv.classList.add("selected")
+    originalCarLot.newDesc(theDiv);
+  },
+//*******Function to mirror user input to car description********//
+
+  
+  originalCarLot.newDesc = function(theDiv){
+    console.log("theDiv", theDiv );
+    let carDesc = theDiv.querySelector("h4");
+  
+
+    inputField.value = "";
+    inputField.focus();
+
+    inputField.addEventListener("keyup", function(){
+      carDesc.innerHTML = inputField.value;
+
+      
+    })
+
   }
+
+
 
 
 return originalCarLot
