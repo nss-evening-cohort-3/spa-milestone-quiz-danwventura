91abd96fc60598af4836065ce3f19fcd23da0a80
Dan Ventura <danwventura@icloud.com>
2016-04-17 21:12:47 -0500

    added spacing between lines

--- quiz.js
+++ quiz.js
@@ -1,3 +1,31 @@
 "use strict"
+//*******Establishing Augmenter*******//  
+var CarLot = (function(originalCarLot){
+//*******populatePage function builds DOM Strings*******//  
+  
+  originalCarLot.populatePage = function(carArray) {
+    let carContainer = document.getElementById("container");
+
+  //*******Loops over inventory and populates DOM*******//  
+    let outputString = "";
+    for (let i = 0; i < carArray.length; i++) {
+      let currentCar = carArray[i]
+      
+  // *************Building DOM String*********** //
+      outputString += `<div id="eachCar${i}" class="col-md-4" style="border-color: ${currentCar.color}">`
+      outputString += `<h2>${currentCar.year} ${currentCar.make} ${currentCar.model}</h2>`
+      outputString += `<h6> Price: ${currentCar.price} </h6>`
+      outputString += `<h5 id="${currentCar.color}"> Color: ${currentCar.color}</h5>`
+      outputString += `<h6> Availability: ${currentCar.purchased}</h6>`
+      outputString += `<h4 id="${currentCar.description}"> Desc: <p>${currentCar.description}</p></h4>`
+      outputString += `</div>`
+      carContainer.innerHTML = outputString;
+  }
+  
+// Calling activateEvents which builds columns/rows
+  CarLot.activateEvents();
+}
+return originalCarLot
+})(CarLot || {});
 
 CarLot.loadInventory(CarLot.populatePage);
